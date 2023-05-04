e8667348cb62d4439d091d97f0f163e4a1bf0af8
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-04 14:32:51 +0200

    Immutability und BookRatingService

--- book-rating/src/app/books/shared/book-rating.service.ts
+++ book-rating/src/app/books/shared/book-rating.service.ts
@@ -9,10 +9,23 @@ export class BookRatingService {
   constructor() { }
 
   rateUp(book: Book): Book {
-    return book; // TODO
+    // Early Exit
+    if (book.rating >= 5) {
+      return book;
+    }
+
+    return {
+      ...book,
+      // rating: book.rating < 5 ? book.rating + 1 : 5
+      // rating: Math.min(book.rating + 1, 5)
+      rating: book.rating + 1
+    };
   }
 
   rateDown(book: Book): Book {
-    return book; // TODO
+    return {
+      ...book,
+      rating: Math.max(book.rating - 1, 1)
+    }
   }
 }
