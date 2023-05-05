d54d8d41a44b9091117ace0e086c739f16e5dcf3
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-05 10:18:30 +0200

    Testrumpf für Dashboard

--- book-rating/src/app/books/dashboard/dashboard.component.spec.ts
+++ book-rating/src/app/books/dashboard/dashboard.component.spec.ts
@@ -22,4 +22,6 @@ describe('DashboardComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call service.rateUp() for doRateUp()', () => {});
 });
