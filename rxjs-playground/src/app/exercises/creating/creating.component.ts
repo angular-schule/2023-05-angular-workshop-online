2a5769633a5389a5e57218bbedce629741706392
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-09 12:19:58 +0200

    RxJS: Observable und Operatoren

--- rxjs-playground/src/app/exercises/creating/creating.component.ts
+++ rxjs-playground/src/app/exercises/creating/creating.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Observable, of, from, timer, interval, ReplaySubject, map, filter } from 'rxjs';
+import { Observable, of, from, timer, interval, ReplaySubject, map, filter, Subscriber, Observer } from 'rxjs';
 import { HistoryComponent } from '../../shared/history/history.component';
 
 @Component({
@@ -23,6 +23,46 @@ export class CreatingComponent {
      */
 
     /******************************/
+
+    // of('Klaus', 'Lisa', 'Kurt', 'Melanie')
+    // from([1,2,3,4,5,6])
+    // interval(1000)     // ---0---1---2---3---4---5---6---7--- ...
+    // timer(3000)        // ---------0|
+    // timer(3000, 1000)  // ---------0---1---2---3---4---5---6---7--- ...
+    // timer(0, 1000)     // 0---1---2---3---4---5---6---7--- ...
+    // Marble-Diagramm
+
+    timer(0, 1000).pipe(
+      map(e => e * 3),
+      filter(e => e % 2 === 0)
+    ).subscribe({
+      next: e => this.log(e),
+      complete: () => this.log('COMPLETE')
+    })
+
+    /******************************/
+
+    function producer(sub: Subscriber<number>) {
+      const result = Math.random();
+      sub.next(result);
+
+      sub.next(10);
+
+      setTimeout(() => sub.next(20), 2000);
+      setTimeout(() => sub.complete(), 5000);
+
+    }
+
+    const obs: Observer<number> = {
+      next: (e) => console.log(e),
+      error: (err: any) => console.error(err),
+      complete: () => console.log('FERTIG!')
+    };
+
+    // producer(obs);
+    const myObs$ = new Observable(producer);
+    // myObs$.subscribe(obs);
+
 
 
     /******************************/
