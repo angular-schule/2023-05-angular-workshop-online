fbbcd58bd908d69034a77915b7cc66bd423c7ec1
Ferdinand Malcher <ferdinand@malcher.media>
2023-05-10 13:33:53 +0200

    NgRx einrichten
    ng add @ngrx/store
    ng add @ngrx/store-devtools
    ng add @ngrx/effects
    ng add @ngrx/schematics@15

--- book-rating/src/app/app.module.ts
+++ book-rating/src/app/app.module.ts
@@ -1,9 +1,12 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { EffectsModule } from '@ngrx/effects';
 
 @NgModule({
   declarations: [
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    StoreModule.forRoot({}, {}),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
+    EffectsModule.forRoot([])
   ],
   providers: [],
   bootstrap: [AppComponent]
